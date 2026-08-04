5b596a6be2e3308c4ccccd55df1a7970bf30ddcf
Ahmed Mohamed <ahmedmohamed1357911@gmail.com>
2026-08-04 22:22:38 +0300

    Refactor welcome.blade.php to streamline HTML structure and improve performance by removing unused styles and scripts.

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+  <!--begin::Head-->
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+
+    @include('layouts.head') 
+
+  </head>
+  <!--end::Head-->
+  <!--begin::Body-->
+  <body class="layout-fixed sidebar-expand-lg bg-body-tertiary">
+    <!--begin::App Wrapper-->
+    <div class="app-wrapper">
+      <!--begin::Header-->
+      @include('layouts.main-headerbar')
+      <!--end::Header-->
+      <!--begin::Sidebar-->
+      @include('layouts.main-sidebar')
+      <!--end::Sidebar-->
+      <!--begin::App Main-->
+      <main class="app-main">
+        <!--begin::App Content Header-->
+        <div class="app-content-header">
+          <!--begin::Container-->
+          <div class="container-fluid">
+            <!--begin::Row-->
+            <div class="row">
+              <div class="col-sm-6">
+                <h1 class="mb-0 fs-3">@yield("main_title")</h1>
+              </div>
+              <div class="col-sm-6">
+                <nav aria-label="breadcrumb">
+                  <ol class="breadcrumb float-sm-end">
+                    <li class="breadcrumb-item"><a href="#">@yield("title_page_1")</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">@yield("title_page_2")</li>
+                  </ol>
+                </nav>
+              </div>
+            </div>
+            <!--end::Row-->
+          </div>
+          <!--end::Container-->
+        </div>
+        <!--end::App Content Header-->
+        <!--begin::App Content-->
+        <div class="app-content">
+          <!--begin::Container-->
+          <div class="container-fluid">
+            <!--begin::Row-->
+            @yield("content")
+
+            <!-- /.row (main row) -->
+          </div>
+          <!--end::Container-->
+        </div>
+        <!--end::App Content-->
+      </main>
+      <!--end::App Main-->
+      <!--begin::Footer-->
+      @include('layouts.footer')
+      <!--end::Footer-->
+    </div>
+    <!--end::App Wrapper-->
+
+    <!-- Start Scripts -->
+    @include('layouts.footer-scripts')
+    <!-- End Scripts -->
+
+  </body>
+  <!--end::Body--> 
+</html>
---
+++ resources/views/new.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.master')
+
+@section('title', 'Main Dashboard')
+
+@section("css")
+
+
+@endsection
+
+@section("title_page_1", "Dashboard")
+
+@section("title_page_2", "Main")
+
+@section("main_title", "Dashboard")
+
+
+@section("content")
+
+
+@endsection
+
+
+@section("scripts")
+
+
+@endsection
